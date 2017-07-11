dbd91c6eca42d9f76fbca014d38cca3d8c64ed80
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-07-11 14:34:26 -0700

    Learning Laravel

---
+++ app/Http/Controllers/mycontroller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class mycontroller extends Controller
+{
+    public function XinChao(){
+        echo 'xin chao cac ban';
+    }
+    public function Course($name){
+        echo 'Course:'.$name;
+    }
+    public function getURL(Request $request){
+        if ( $request->isMethod('get'))
+            echo "get method";
+        else echo "not get method";
+    }
+    public function getForm(Request $request){
+       echo $request->url();
+    }
+    public function postForm(Request $request){
+        echo 'Ten toi la: ';
+        echo $request->input('HoTen');
+    }
+    /*WORKING WITH COOKIE*/
+    public function setCookie(){
+        $response = new Response();
+        $response->withCookie('khoahoc', 'laravel', '0.5');
+        echo 'da set cookie';
+        return $response;
+
+    }
+    public function getCookie(Request $request){
+        echo 'da get cookie';
+        return $request->cookie('khoahoc');
+    }
+
+    /*UPLOAD FILE TO CODING FOLDER PUBLIC->IMAGES*/
+    public function postFile(Request $request){
+        if($request->hasFile('myFile'))                     //kiểm tra xem có file hay không
+        {
+            $file = $request->file('myFile');               // quét file
+            if($file->getClientOriginalExtension()=='jpg'){ //check right file extention
+                $filename = $file->getClientOriginalName(); //đặt tên file đứng với gốc
+                $file->move('images',$filename);            // save file at location 'images'
+                echo 'have uploaded file';
+            }
+            else{
+                echo 'not compatible file';
+            }
+
+        }
+        else echo 'non any file';
+    }
+
+    /*CREATED FILE .JSON*/
+    public function getJson(){
+        $array=['php','html','css','javascript'];
+        return response()->json($array);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('route1',['as'=>'Myroute',function(){
+    echo 'xin chao cac bans';
+}]);
+Route::get('route2',function (){
+  echo 'day la route 2';
+})->name('myroute2');
+Route::get('goiten',function (){
+   return redirect()->route('myroute2');
+});
+// call controller
+Route::get('callcontroler','mycontroller@XinChao');
+Route::get('parameter/{ten}','mycontroller@Course');
+Route::get('request','mycontroller@getURL');
+
+Route::get('getForm',function (){
+    return view('postForm');
+});
+Route::post('postForm',['as'=>'postForm','uses'=>'mycontroller@postForm']);
+
+/*Cookies*/
+Route::get('setCookie','mycontroller@setCookie');
+Route::get('getCookie','mycontroller@getCookie');
+
+/**
+ * UPLOAD FILE USED REQUEST
+ */
+Route::get('uploadFile',function (){
+    return view('postFile');
+});
+Route::post('postFile',['as'=>'postFile','uses'=>'mycontroller@postFile']);
+
+// CREATED FILE .JSON
+Route::get('getJson','mycontroller@getJson');
